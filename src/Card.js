552e4288a1f69d75c0c9690bb15fb8300dc78b2b
Prem Acharya <prem.acharya@hotmail.com>
2018-07-09 21:49:06 +1000

    first commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+        return (
+        <div  className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-2 tc'>
+            <img alt='robort' src='https://robohash.org/prem?size=100x100' />
+            <div>
+                hellow there
+            </div>
+            <p>
+                emails@yahoo.com
+            </p>
+        </div>
+    )
+}
+    
+
+export default Card
