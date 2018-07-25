a1baf543b9c333dacd96362cb634cb9eb30360a1
Prem Acharya <prem.acharya@hotmail.com>
2018-07-26 02:29:17 +1000

    ErrorBoundry component for card is added

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css'
 
 
@@ -20,7 +21,7 @@ class App extends Component {
     }
 
     componentDidMount(){
-        const users = fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users')
          .then(response=>{ return response.json() })
          .then(users => {this.setState({ robots: users })})
         
@@ -39,7 +40,9 @@ class App extends Component {
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filterRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filterRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
              )
