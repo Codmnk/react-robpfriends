96c18f95db6a817347cad34455df75c9eb0ec19b
Prem Acharya <prem.acharya@hotmail.com>
2018-07-25 22:42:24 +1000

    card component added

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({name, email, id}) => {
+    
         return (
         <div  className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-2 tc'>
-            <img alt='robort' src='https://robohash.org/prem?size=100x100' />
+            <img alt='robort' src={`https://robohash.org/${id}?size=100x100`} />
             <div>
-                hellow there
-            </div>
+                {name}
+            </div>     
             <p>
-                emails@yahoo.com
+                {email}
             </p>
         </div>
     )
---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'prem',
+        username: 'bret',
+        email: 'email@testemail.com'
+    },
+    {
+        id: 2,
+        name: 'rob',
+        username: 'brdsfdset',
+        email: 'email@dds.com'
+    },
+    {
+        id: 3,
+        name: 'rob',
+        username: 'brdsfdset',
+        email: 'email@dds.com'
+    },
+    {
+        id: 1,
+        name: 'prem',
+        username: 'bret',
+        email: 'email@testemail.com'
+    },
+    {
+        id: 2,
+        name: 'rob',
+        username: 'brdsfdset',
+        email: 'email@dds.com'
+    },
+    {
+        id: 3,
+        name: 'rob',
+        username: 'brdsfdset',
+        email: 'email@dds.com'
+    }
+]
