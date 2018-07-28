9512dcd94dfc6dee36d6aed0d1ec581f8a09d923
Prem Acharya <prem.acharya@hotmail.com>
2018-07-28 21:58:36 +1000

    redux added search field action and reducer added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css'
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 
-    onSearchChange = (event)=>{
-        this.setState({searchfield: event.target.value})
-        
-    }
 
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -28,9 +38,10 @@ class App extends Component {
     }
 
     render(){
-        const { robots, searchfield} = this.state
+        const { robots } = this.state
+        const { searchField, onSearchChange } = this.props
         const filterRobots = robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
 
         return !robots.length ?
@@ -38,7 +49,7 @@ class App extends Component {
              (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filterRobots} />
@@ -51,4 +62,4 @@ class App extends Component {
     
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App)
